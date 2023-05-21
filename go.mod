c90d320abdbe3045ebd5fa8c4e650721d41752b6
xiaoxiang10086 <22251227@zju.edu.cn>
2023-05-21 22:44:19 +0800

    feat: Add auto-injection support for Layotto container at namespace scope

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module mutationwebhook
 
 go 1.16
 
-require k8s.io/api v0.27.2
+require (
+	k8s.io/api v0.27.2
+	k8s.io/apimachinery v0.27.2
+	k8s.io/client-go v0.27.2
+)
--- main.go
+++ main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"log"
 	"net/http"
 
 	"k8s.io/api/admission/v1beta1"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
 	CertFile = "/etc/webhook/certs/tls.crt"
 	KeyFile  = "/etc/webhook/certs/tls.key"
 )
+
+var clientset *kubernetes.Clientset
 
 // Webhook implements a mutating webhook for automatic container injection.
 type Webhook struct {
@@ -54,6 +60,12 @@ func NewWebhook(p WebhookParameters) (*Webhook, error) {
 		cert:     &pair,
 	}
 
+	clientset, err = CreateKubernetesClient()
+	if err != nil {
+		log.Fatalf("Failed to create Kubernetes client: %v", err)
+		return wh, err
+	}
+
 	mux := http.NewServeMux()
 	wh.server = &http.Server{
 		Addr:      fmt.Sprintf(":%v", p.Port),
@@ -71,6 +83,16 @@ func (wh *Webhook) Run() {
 	}
 }
 
+func CreateKubernetesClient() (*kubernetes.Clientset, error) {
+	// create a Kubernetes client config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		log.Fatalf("Failed to create Kubernetes client config: %v", err)
+	}
+
+	return kubernetes.NewForConfig(config)
+}
+
 func main() {
 	parameters := WebhookParameters{
 		CertFile: CertFile,
@@ -81,6 +103,7 @@ func main() {
 	wh, err := NewWebhook(parameters)
 	if err != nil {
 		fmt.Errorf("failed to create mutate webhook: %v", err)
+		return
 	}
 
 	wh.Run()
@@ -127,29 +150,56 @@ func (wh *Webhook) serveMutate(w http.ResponseWriter, r *http.Request) {
 func createPatch(pod *corev1.Pod) ([]byte, error) {
 	var patch []patchOperation
 
-	labels := pod.GetLabels()
-	if labels != nil {
-		if value, ok := labels["ossp"]; ok && value == "2023" {
-			container := corev1.Container{
-				Name:  "injected-container",
-				Image: "busybox",
-				Command: []string{
-					"sh",
-					"-c",
-					"echo 'Hello from the injected container!' && sleep 3600",
-				},
-			}
-			pod.Spec.Containers = append(pod.Spec.Containers, container)
+	if shouldInjectSidecar(pod) {
+		fmt.Printf("should inject sidecar\n")
 
-			patch = append(patch, patchOperation{
-				Op:    "add",
-				Path:  "/spec/containers/-",
-				Value: container,
-			})
+		container := corev1.Container{
+			Name:  "injected-container",
+			Image: "busybox",
+			Command: []string{
+				"sh",
+				"-c",
+				"echo 'Hello from the injected container!' && sleep 3600",
+			},
 		}
+		pod.Spec.Containers = append(pod.Spec.Containers, container)
+
+		patch = append(patch, patchOperation{
+			Op:    "add",
+			Path:  "/spec/containers/-",
+			Value: container,
+		})
+
+	} else {
+		fmt.Printf("should not inject sidecar\n")
 	}
 
 	return json.Marshal(patch)
+}
+
+func shouldInjectSidecar(pod *corev1.Pod) bool {
+	labels := pod.GetLabels()
+	namespace := pod.GetNamespace()
+
+	if labels != nil {
+		if value, ok := labels["mosn.io/sidecar-inject"]; ok && value == "false" {
+			return false
+		}
+	}
+
+	ns, err := clientset.CoreV1().Namespaces().Get(context.Background(), namespace, metav1.GetOptions{})
+	if err != nil {
+		log.Printf("Failed to get namespace %s: %v\n", namespace, err)
+		return false
+	}
+
+	if ns.Labels != nil {
+		if value, ok := ns.Labels["mosn.io/layotto-inject"]; ok && value == "true" {
+			return true
+		}
+	}
+
+	return false
 }
 
 type patchOperation struct {
